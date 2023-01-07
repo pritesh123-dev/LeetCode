0a331f6f2307c5e096bd49d2fb688ec0ea4aa1d1
pritesh123-dev <85010520+pritesh123-dev@users.noreply.github.com>
2023-01-07 07:14:51 +0530

    Time: 74 ms (98.61%), Space: 44.8 MB (73.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -5,8 +5,8 @@
 var setZeroes = function(matrix) {
     var arr = [];
 
-    for(var i = 0; i < matrix.length; ++i) {
-        for(var j = 0; j < matrix[i].length; ++j) {
+    for(var i = 0; i < matrix.length; i++) {
+        for(var j = 0; j < matrix[i].length; j++) {
             if(matrix[i][j] == 0) {
                 arr.push(i);
                 arr.push(j);
@@ -14,13 +14,13 @@ var setZeroes = function(matrix) {
         }
     }
 
-    for(var z = 0; z < arr.length; ++z) {
-        for(j = 0; j < matrix[arr[z]].length; ++j) {
+    for(var z = 0; z < arr.length; z++) {
+        for(j = 0; j < matrix[arr[z]].length; j++) {
             matrix[arr[z]][j] = 0;
         }
-        for(i = 0; i < matrix.length; ++i) {
+        for(i = 0; i < matrix.length; i++) {
             matrix[i][arr[z+1]] = 0;
         }
-        ++z;
+        z++;
     }
 };
