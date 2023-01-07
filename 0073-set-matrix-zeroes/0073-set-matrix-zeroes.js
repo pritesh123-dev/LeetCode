ade98a64006d8103c6232517d82ba73521aa9d7d
pritesh123-dev <85010520+pritesh123-dev@users.noreply.github.com>
2023-01-07 07:33:32 +0530

    Time: 89 ms (78.63%), Space: 44.9 MB (57.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,21 +3,22 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    var arr = [];
-
-    for(var i = 0; i < matrix.length; i++) {
-        for(var j = 0; j < matrix[i].length; j++) {
+    var arr = [];  
+    for(let i = 0 ; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
             if(matrix[i][j] == 0) {
                 arr.push(i);
                 arr.push(j);
             }
         }
     }
-
-    for(var z = 0; z < arr.length; z++) {
+    
+    for(let z = 0; z < arr.length; z++) {
+        
         for(j = 0; j < matrix[arr[z]].length; j++) {
             matrix[arr[z]][j] = 0;
         }
+        
         for(i = 0; i < matrix.length; i++) {
             matrix[i][arr[z+1]] = 0;
         }
