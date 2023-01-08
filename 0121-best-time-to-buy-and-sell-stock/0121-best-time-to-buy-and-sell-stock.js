dc9f83aab1d0dd7d64ba63532fdf8080afc4cd70
pritesh123-dev <85010520+pritesh123-dev@users.noreply.github.com>
2023-01-08 15:04:44 +0530

    Time: 81 ms (96.18%), Space: 51.8 MB (54.69%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minVal = prices[0];
+    let profit = 0;
+    
+    for(let i = 0; i < prices.length; i++) {
+        if(prices[i] < minVal) {
+            minVal = prices[i];
+        }
+        else if(prices[i] - minVal > profit) {
+            profit = prices[i] - minVal;
+        }
+    }
+    return profit;
+};
