446ed957c793fce18462fed429599140d0bcb22b
pritesh123-dev <85010520+pritesh123-dev@users.noreply.github.com>
2023-01-08 08:06:04 +0530

    Time: 91 ms (77.58%), Space: 50.4 MB (44.86%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+    let maxVal = nums[0];
+    let sum = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if(sum > maxVal) {
+            maxVal = sum;
+        }
+        if(sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxVal;
+};
